9dd2c3a01a96c5e08e4eb3eb13aa6c6f1ff175a9
Joy Chace <willkerneljc@gmail.com>
2017-12-05 18:02:56 +0800

    git add files

---
+++ app/src/free/java/com/willkernel/app/gradledsl/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.willkernel.app.gradledsl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.willkernel.app.gradledsl.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
